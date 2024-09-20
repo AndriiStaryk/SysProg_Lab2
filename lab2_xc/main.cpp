3fb6289a40b64037d533bea42c7326af95e0de57
Andrii_Mac <andrystarik@gmail.com>
2024-09-20 12:01:56 +0300

    Initial Commit

---
+++ lab2_xc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab2_xc
+//
+//  Created by Andrii Staryk on 20.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
